86adedf29e8a5a05538cf3e861b9a18d2114cd45
Alex Clemmer <clemmer.alexander@gmail.com>
2015-01-01 01:11:22 -0800

    Initial commit with working CRC code

---
+++ bzip2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+const CrcInitVal uint32 = 0xffffffff
+var CrcTable [256]uint32
+
+func updateCrc(crcVar uint32, cha byte) uint32 {
+	crcVar = (crcVar << 8) ^ CrcTable[(crcVar >> 24) ^ uint32(cha)]
+	return crcVar
+}
+
+func finalizeCrc(crcVar uint32) uint32 {
+	crcVar = ^(crcVar)
+	return crcVar
+}
+
+func doCrc(text []byte) uint32 {
+	crc := CrcInitVal
+	for _, b := range text {
+		crc = updateCrc(crc, b)
+	}
+	return crc
+}
+
+func main() {
+	CrcTable = crcTable()
+	data, _ := ioutil.ReadFile("data/gutenburg20.txt")
+	text := string(data)
+
+	crc := doCrc(data)
+	crc = finalizeCrc(crc)
+
+	fmt.Println(text)
+	fmt.Printf("0x%x\n", crc)
+}
